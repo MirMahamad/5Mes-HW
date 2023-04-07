0386e80c918e490f4290c342491f05e3c4715580
MirMahamad <the.aman1414@gmail.com>
2023-04-08 02:42:05 +0600

    'hw5'

--- shop_api/urls.py
+++ shop_api/urls.py
@@ -1,14 +1,9 @@
 from django.contrib import admin
-from django.urls import path
-from product import views
+from django.urls import path, include
+
 
 urlpatterns = [
-    path('admin/', admin.site.urls),
-    path('api/v1/categories/', views.categories_list_api_view),
-    path('api/v1/categories/<int:id>/', views.categories_detail_api_view),
-    path('api/v1/products/', views.products_list_api_view),
-    path('api/v1/products/<int:id>/', views.products_detail_api_view),
-    path('api/v1/reviews/', views.reviews_list_api_view),
-    path('api/v1/reviews/<int:id>/', views.reviews_detail_api_view),
-    path('api/v1/products/reviews/', views.products_reviews_api_view),
+        path("admin/", admin.site.urls),
+        path("", include("product.urls")),
+        path("", include("user.urls")),
 ]
